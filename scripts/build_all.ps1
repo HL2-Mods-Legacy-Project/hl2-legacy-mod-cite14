0c2ab905180cacc0b13b7bb5010701fab0e13954
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 19:46:20 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Cite14_0.2" `
+  -ModName "Cite 14 0.2" `
+  -ModFolder "Cite 14" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/cite14/Cite-14-0.2-Original.zip" `
+  -ModBaseFilesUrlHash "de0c5d11c4d31b109ff8d4df577f8e3774f1a6490bc9a555a1d651d1884f9035" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
